3b9dbdfdcb0f75a01ea04b20a90a82dce915255d
Ricardo Pacheco <rjgpacheco@gmail.com>
2021-01-09 20:25:31 +0000

    More methods

--- myutils/dictionaries.py
+++ myutils/dictionaries.py
@@ -3,6 +3,8 @@ A collection of utility functions to work with dictionaries.
 """
 from copy import deepcopy
 from typing import Union
+from typing import Sequence
+
 
 def delete_field(dictionary: dict, field, inplace=True) -> Union[dict, None]:
     """
@@ -11,7 +13,7 @@ def delete_field(dictionary: dict, field, inplace=True) -> Union[dict, None]:
     Dictionary is modified in palce if `inplace=True` (default)
     """
 
-    # Safe copy
+    #  Safe copy
     if not inplace:
         dictionary = deepcopy(dictionary)
 
@@ -22,7 +24,7 @@ def delete_field(dictionary: dict, field, inplace=True) -> Union[dict, None]:
         if isinstance(v, dict):
             delete_field(v, field, inplace=True)
 
-    # If not inplace, return
+    #  If not inplace, return
     if not inplace:
         return dictionary
 
@@ -49,3 +51,67 @@ def filter_fields(dictionary: dict, fields: list, inplace=True) -> Union[dict, N
     #  If not inplace, return
     if not inplace:
         return dictionary
+
+
+def check_dict_has_kv(dictionary: dict, key, value) -> bool:
+    """
+    Check if `dictionary` has entry `key` with value `value`.
+    """
+    if key in dictionary.keys() and dictionary[key] == value:
+        return True
+    return False
+
+def check_dict_kv_in_list(dictionary: dict, key, values:Sequence) -> bool:
+    """
+    Check if `dictionary` has entry `key` with value in `values`.
+    """
+    if key in dictionary.keys() and dictionary[key] in values:
+        return True
+    return False
+
+def merge_dicts_kv(dictionaries: Sequence[dict]) -> dict:
+    """
+    Merges a list of dictionaries to a single dictionary. 
+
+    For keys shared between dictionaries, their values will be concatenated.
+
+    All values in output dictionary are lists.
+    """
+    dictionary = {}
+    for i, subdict in enumerate(dictionaries):
+        if not isinstance(subdict, dict):
+            raise ValueError(f"dictionaries[{i}] is of type '{type(subdict)}'")
+
+        for k, v in subdict.items():
+            if isinstance(v, dict):
+                raise ValueError("Supplied dictionaries must not have dictionaries as values.")
+
+            if k not in dictionary.keys():
+                dictionary[k] = []
+
+            dictionary.get(k, []).append(v)
+    return dictionary
+
+
+def delete_subdicts(dictionary: dict, dicts: Sequence[dict], inplace=True) -> Union[dict, None]:
+    """
+    Deletes sub dictionaires present as values in `dictionary`, if they match a dict in dicts
+    """
+    #  Safe copy
+    if not inplace:
+        dictionary = deepcopy(dictionary)
+
+    #  Merge all dicts as k -> Sequence(keys) so we don't need to iterate over all of them
+    to_exclude = merge_dicts_kv(dicts)
+    for k, v in list(dictionary.items()):
+        if not isinstance(v, dict):
+            continue
+
+        for badkey, badvalues in to_exclude.items():
+            if badkey in list(v.keys()) and v[badkey] in badvalues:
+                del dictionary[k]
+
+    #  If not inplace, return
+    if not inplace:
+        return dictionary
+
