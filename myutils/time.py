1ef12555bf3b169cd4b4900553b35740e7a93fdb
Ricardo Pacheco <rjgpacheco@gmail.com>
2021-01-10 01:12:44 +0000

    Added time utils

--- myutils/time.py
+++ myutils/time.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 
 
-def time_to(target, format="%Y-%m-%d %H:%M:%S"):
+def time_to(target, timeformat="%Y-%m-%d %H:%M:%S"):
     """
     Get number of seconds to target time.
     """
-    return datetime.strptime(target, format=format) - datetime.now().total_seconds()
+    return datetime.strptime(target, timeformat=timeformat) - datetime.now().total_seconds()
 
-    
