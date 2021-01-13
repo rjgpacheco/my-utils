5f5bd709f3056472db93bdd95c140fd45ba48ea4
Ricardo Pacheco <rjgpacheco@gmail.com>
2021-01-13 21:34:58 +0000

    Added sleep_until function

--- myutils/time.py
+++ myutils/time.py
@@ -11,9 +11,11 @@ def time_to(target, timeformat="%Y-%m-%d %H:%M:%S"):
 def sleep_until(target, timeformat="%Y-%m-%d %H:%M:%S", silent=False):
     remaining = time_to(target=target, timeformat=timeformat)
     if remaining <= 0:
-        print("Specified time aleady passed.")
+        if not silent:
+            print("Specified time aleady passed.")
     else:
-        print(f"Sleeping for {remaining} seconds, until {target}")
+        if not silent:
+            print(f"Sleeping for {remaining} seconds, until {target}")
         sleep(remaining)
 
 
