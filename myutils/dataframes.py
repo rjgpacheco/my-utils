6e7b8a8aed975e52e528570ee4cfa81d7ae28818
Ricardo Pacheco <rjgpacheco@gmail.com>
2021-01-24 16:12:20 +0000

    Added column width to display options

--- myutils/dataframes.py
+++ myutils/dataframes.py
@@ -5,6 +5,7 @@ Some are just ways I like calling specific functions.
 """
 
 import pandas as pd
+from pyperclip import copy
 
 
 def save_dataframe(filepath: str, df) -> None:
@@ -15,10 +16,21 @@ def load_dataframe(filepath: str):
     return pd.read_csv(filepath, sep="\t")
 
 
-def print_df(df, max_rows=None, max_cols=None):
+def print_df(df, max_rows=None, max_cols=None, max_colwidth=None):
     """
     This is used in Jupyter notebooks or IPython consoles 
     to print more of a dataframe.
     """
-    with pd.option_context("display.max_rows", max_rows, "display.max_columns", max_cols):
+    with pd.option_context(
+        "display.max_rows", max_rows,
+         "display.max_columns", max_cols,
+         'display.max_colwidth', max_colwidth,
+         ):
         display(df)  # pylint: disable=E0602
+
+
+def copy_dataframe_clipboard(df):
+    """
+    Copy dataframe to clipboard in TSV format.
+    """
+    copy(df.to_csv(sep="\t", index=False))
