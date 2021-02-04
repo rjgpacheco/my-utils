c529680a329556fd830098152b4b7305444eea5f
Ricardo Pacheco <rjgpacheco@gmail.com>
2021-02-04 18:00:01 +0000

    Added file hashing

--- myutils/utils.py
+++ myutils/utils.py
@@ -4,6 +4,7 @@ Collection of utilities I use often.
 Some are just ways I like calling specific functions.
 """
 
+import hashlib
 import random
 import re
 from typing import Sequence
@@ -130,4 +131,25 @@ def pretty_round(x, precision=0):
         return "<0." + "0" * (precision - 1) + "1%"
 
     return s
-    
+
+
+def get_bytes_hash(bytes, hash_function=hashlib.md5, chunk_size=4096):
+    """
+    Return hash of byte stream.
+
+    Adapted from quantumSoup at https://stackoverflow.com/a/3431838.
+    """
+    hash = hash_function()
+    for chunk in iter(lambda: bytes.read(chunk_size), b""):
+        hash.update(chunk)
+
+    return hash
+
+
+def get_file_hash(file_path, hash_function=hashlib.md5, chunk_size=4096):
+    """
+    Return hash of file contents.
+    """
+    with open(file_path, "rb") as f:
+        return get_bytes_hash(f, hash_function=hash_function, chunk_size=chunk_size)
+
